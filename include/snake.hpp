f63300e3322e1c8c033bfb4c899575626ce3909f
ReZerVV <kirillmorozov547@gmail.com>
2023-04-05 11:23:26 +0300

    Created a snake class.

---
+++ include/snake.hpp
@@ -0,0 +1,26 @@
+#ifndef SNAKE_H
+#define SHAKE_H
+
+#include <vector>
+#include <glm/glm.hpp>
+
+class snake {
+public:
+	snake();
+	snake(const snake&) = default;
+	snake(snake&&) = default;
+	~snake();
+public:
+	size_t size() const { 
+		return this->_body.size();
+	}
+	void move(const glm::vec2& direction) {
+		this->_direction = direction;
+	}
+public:
+	std::vector<glm::vec2> 	_body;
+	std::vec2 				_direction;
+	size_t 					_points;
+};
+
+#endif
---
+++ source/snake.cpp
@@ -0,0 +1,8 @@
+#include "snake.hpp"
+
+snake::snake()
+	:
+	_direction(0.0f, 0.0f),
+	_points(0) {
+	this->_body.push_back(glm::vec2(0.0f, 0.0f));
+}
